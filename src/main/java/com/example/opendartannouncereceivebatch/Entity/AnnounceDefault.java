1fcab04b80e775b2675dd5977a64b3efe07de323
kimminseok <bjm7701@naver.com>
2023-09-15 23:57:10 +0900

    테이블 프로퍼티 camelCase 변경(기존 테이블은 그냥 쓰는걸로,,)

--- src/main/java/com/example/opendartannouncereceivebatch/Entity/AnnounceDefault.java
+++ src/main/java/com/example/opendartannouncereceivebatch/Entity/AnnounceDefault.java
@@ -10,7 +10,7 @@ import org.springframework.data.annotation.Id;
 import java.time.LocalDate;
 
 
-@Entity(name = "announceDefault")
+@Entity(name = "CorpCommonAnnouncement")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
