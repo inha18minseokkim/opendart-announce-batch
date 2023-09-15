abe3798fd78c3556ac9c0d9c5dfd60868233f0e4
kimminseok <bjm7701@naver.com>
2023-09-15 23:56:23 +0900

    annotation 추가

--- src/main/java/com/example/opendartannouncereceivebatch/Repository/AnnounceDefaultRepository.java
+++ src/main/java/com/example/opendartannouncereceivebatch/Repository/AnnounceDefaultRepository.java
@@ -2,6 +2,8 @@ package com.example.opendartannouncereceivebatch.Repository;
 
 import com.example.opendartannouncereceivebatch.Entity.AnnounceDefault;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnnounceDefaultRepository extends JpaRepository<AnnounceDefault, Long> {
 }
