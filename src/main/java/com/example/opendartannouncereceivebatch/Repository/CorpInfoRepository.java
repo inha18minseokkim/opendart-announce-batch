d5473a1151e081bbda468cd426469b31bec077be
kimminseok <bjm7701@naver.com>
2023-09-15 22:59:22 +0900

    기업코드정보 JPARepository 추가

---
+++ src/main/java/com/example/opendartannouncereceivebatch/Repository/CorpInfoRepository.java
@@ -0,0 +1,7 @@
+package com.example.opendartannouncereceivebatch.Repository;
+
+import com.example.opendartannouncereceivebatch.Entity.CorpInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CorpInfoRepository extends JpaRepository<CorpInfo, Long> {
+}
