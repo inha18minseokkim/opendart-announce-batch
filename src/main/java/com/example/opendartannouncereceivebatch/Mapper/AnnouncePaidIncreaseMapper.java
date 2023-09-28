a447fe6c944fb962e3040a6ad0d2249ed4d7f4df
kimminseok <bjm7701@naver.com>
2023-09-28 17:15:22 +0900

    매퍼 수정

--- src/main/java/com/example/opendartannouncereceivebatch/Mapper/AnnouncePaidIncreaseMapper.java
+++ src/main/java/com/example/opendartannouncereceivebatch/Mapper/AnnouncePaidIncreaseMapper.java
@@ -11,27 +11,25 @@ import org.mapstruct.*;
 )
 public interface AnnouncePaidIncreaseMapper {
     @Mappings({
-            @Mapping(target = "receptNumber", expression="java(java.lang.Long.parseLong(announceDefaultElement.getRcept_no()))"), // 접수번호
-            @Mapping(target = "corpClass", source = "corp_cls"), // 법인구분
-            @Mapping(target = "corpCode", source = "corp_code"), // 고유번호
-            @Mapping(target = "corpName", source = "corp_name"), // 회사명
-            @Mapping(target = "newNormalKindCount", expression="java(java.lang.Long.parseLong(element.getNstk_ostk_cnt()))"), // 신주의 종류와 수(보통주식 (주))
-            @Mapping(target = "newEtcKindCount", expression = "java(java.lang.Long.parseLong(element.getNstk_estk_cnt()))"), // 신주의 종류와 수(기타주식 (주))
-            @Mapping(target = "farValue", expression = "java(java.lang.Long.parseLong(element.getFv_ps()))"), // 1주당 액면가액 (원)
-            @Mapping(target = "capitalStockBeforeIncrease", expression = "java(java.lang.Long.parseLong(element.getBfic_tisstk_ostk()))"), // 증자전 발행주식총수 (주)(보통주식 (주))
-            @Mapping(target = "capitalEtcStockBeforeIncrease", expression = "java(java.lang.Long.parseLong(element.getBfic_tisstk_estk()))"), // 증자전 발행주식총수 (주)(기타주식 (주))
-            @Mapping(target = "fundForEquipment", expression = "java(java.lang.Long.parseLong(element.getFdpp_fclt()))"), // 자금조달의 목적(시설자금 (원))
-            @Mapping(target = "fundForTransfer", expression = "java(java.lang.Long.parseLong(element.getFdpp_bsninh()))"), // 자금조달의 목적(영업양수자금 (원))
-            @Mapping(target = "fundForOperation", expression = "java(java.lang.Long.parseLong(element.getFdpp_op()))"), // 자금조달의 목적(운영자금 (원))
-            @Mapping(target = "fundForRedemption", expression = "java(java.lang.Long.parseLong(element.getFdpp_dtrp()))"), // 자금조달의 목적(채무상환자금 (원))
-            @Mapping(target = "fundForSecurityAcquisition", expression = "java(java.lang.Long.parseLong(element.getFdpp_ocsa()))"), // 자금조달의 목적(타법인 증권 취득자금 (원))
-            @Mapping(target = "fundForEtc", expression = "java(java.lang.Long.parseLong(element.getFdpp_etc()))"), // 자금조달의 목적(기타자금 (원))
-            @Mapping(target = "increaseMethod", source = "ic_mthn"), // 증자방식
-            @Mapping(target = "shortSellingStatus", source = "ssl_at"), // 공매도 해당여부
-            @Mapping(target = "shortSellingBeginDate", expression = "java(java.time.LocalDate.parse(element.getSsl_bgd(), " +
-                    "java.time.format.DateTimeFormatter.ofPattern(\\\"yyyyMMdd\\\")))\""), // 공매도 시작일
-            @Mapping(target = "shortSellingEndDate", expression = "java(java.time.LocalDate.parse(element.getSsl_edd(), " +
-                    "java.time.format.DateTimeFormatter.ofPattern(\\\"yyyyMMdd\\\")))\"") // 공매도 종료일
+            @Mapping(target = "receptNumber", expression="java(java.lang.Long.parseLong(element.getRcept_no()))"),
+            @Mapping(target = "corpClass", source = "corp_cls"),
+            @Mapping(target = "corpCode", source = "corp_code"),
+            @Mapping(target = "corpName", source = "corp_name"),
+            @Mapping(target = "newNormalKindCount", expression="java(java.lang.Long.parseLong(element.getNstk_ostk_cnt()))"),
+            @Mapping(target = "newEtcKindCount", expression = "java(java.lang.Long.parseLong(element.getNstk_estk_cnt()))"),
+            @Mapping(target = "farValue", expression = "java(java.lang.Long.parseLong(element.getFv_ps()))"),
+            @Mapping(target = "capitalStockBeforeIncrease", expression = "java(java.lang.Long.parseLong(element.getBfic_tisstk_ostk()))"),
+            @Mapping(target = "capitalEtcStockBeforeIncrease", expression = "java(java.lang.Long.parseLong(element.getBfic_tisstk_estk()))"),
+            @Mapping(target = "fundForEquipment", expression = "java(java.lang.Long.parseLong(element.getFdpp_fclt()))"),
+            @Mapping(target = "fundForTransfer", expression = "java(java.lang.Long.parseLong(element.getFdpp_bsninh()))"),
+            @Mapping(target = "fundForOperation", expression = "java(java.lang.Long.parseLong(element.getFdpp_op()))"),
+            @Mapping(target = "fundForRedemption", expression = "java(java.lang.Long.parseLong(element.getFdpp_dtrp()))"),
+            @Mapping(target = "fundForSecurityAcquisition", expression = "java(java.lang.Long.parseLong(element.getFdpp_ocsa()))"),
+            @Mapping(target = "fundForEtc", expression = "java(java.lang.Long.parseLong(element.getFdpp_etc()))"),
+            @Mapping(target = "increaseMethod", source = "ic_mthn"),
+            @Mapping(target = "shortSellingStatus", source = "ssl_at"),
+            @Mapping(target = "shortSellingBeginDate", expression = "java(java.time.LocalDate.parse(element.getSsl_bgd(),java.time.format.DateTimeFormatter.ofPattern(\"yyyyMMdd\")))"),
+            @Mapping(target = "shortSellingEndDate", expression = "java(java.time.LocalDate.parse(element.getSsl_edd(),java.time.format.DateTimeFormatter.ofPattern(\"yyyyMMdd\")))")
     })
     AnnouncePaidIncrease from(AnnouncePaidIncreaseElement element);
 }
