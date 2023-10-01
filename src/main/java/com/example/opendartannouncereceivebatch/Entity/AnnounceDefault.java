b4f58feb26ecdcf97b191c9f1b007d528bff032d
kimminseok <bjm7701@naver.com>
2023-10-01 15:18:43 +0900

    Getter 추가

--- src/main/java/com/example/opendartannouncereceivebatch/Entity/AnnounceDefault.java
+++ src/main/java/com/example/opendartannouncereceivebatch/Entity/AnnounceDefault.java
@@ -4,10 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -17,6 +14,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 @ToString
+@Getter
 public class AnnounceDefault {
 
     @Id
