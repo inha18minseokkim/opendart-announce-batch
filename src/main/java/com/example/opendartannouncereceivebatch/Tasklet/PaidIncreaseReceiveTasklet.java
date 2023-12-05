03cbff40c9db67f870173ec901a2e6026e9f2f48
kimminseok <bjm7701@naver.com>
2023-12-05 22:37:34 +0900

    주석 달았다

--- src/main/java/com/example/opendartannouncereceivebatch/Tasklet/PaidIncreaseReceiveTasklet.java
+++ src/main/java/com/example/opendartannouncereceivebatch/Tasklet/PaidIncreaseReceiveTasklet.java
@@ -2,7 +2,7 @@ package com.example.opendartannouncereceivebatch.Tasklet;
 
 import com.example.opendartannouncereceivebatch.Business.EssentialApiReceive;
 import com.example.opendartannouncereceivebatch.Code.AnnounceKindCode;
-import com.example.opendartannouncereceivebatch.DTO.EssentialResponseElement;
+import com.example.opendartannouncereceivebatch.DTO.ListElement.EssentialResponseElement;
 import com.example.opendartannouncereceivebatch.Entity.AnnounceDefault;
 import com.example.opendartannouncereceivebatch.Entity.EssentialReport;
 import com.example.opendartannouncereceivebatch.Reader.AnnounceDefaultReader;
@@ -29,22 +29,22 @@ public class PaidIncreaseReceiveTasklet implements Tasklet {
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         String beginDate = applicationArguments.getOptionValues("beginDate").get(0);
         String endDate = applicationArguments.getOptionValues("endDate").get(0);
-
+        //그냥 공시에서 주요사항,증자,유상 이라는 제목을 가지고 있는 공시정보들을 가져옴
         Stream<AnnounceDefault> announceStream = announceDefaultReader.getAnnounceList(beginDate, endDate)
-                .filter((AnnounceDefault element) -> element.getReportName().contains("주요사항"))
-                .filter((AnnounceDefault element) -> element.getReportName().contains("증자"))
-                .filter((AnnounceDefault element) -> element.getReportName().contains("유상"));
-
+                .filter((AnnounceDefault element) -> element.getReport_nm().contains("주요사항"))
+                .filter((AnnounceDefault element) -> element.getReport_nm().contains("증자"))
+                .filter((AnnounceDefault element) -> element.getReport_nm().contains("유상"));
+        //회사코드를 추출해서 그걸로 유상증자 enum 멤버의 uri로 찌름
         Stream<? extends EssentialResponseElement> elementStream = announceStream.flatMap((AnnounceDefault element) -> {
-                log.info(element.getCorpCode() + " : " + element.getReportName() + " 에 대한 호출 진행");
-                return essentialApiReceive.getEssentialAnnouncement(beginDate, endDate, element.getCorpCode(), AnnounceKindCode.PAIDINCREASE);
+                log.info(element.getCorp_code() + " : " + element.getReport_nm() + " 에 대한 호출 진행");
+                return essentialApiReceive.getEssentialAnnouncement(beginDate, endDate, element.getCorp_code(), AnnounceKindCode.PAID_INCREASE);
                 }
         ).map(EssentialResponseElement::getRefinedElement);
-
-        Stream<? extends EssentialReport> entityStream = essentialApiReceive.convertToEntity(elementStream, AnnounceKindCode.PAIDINCREASE);
-
-        Integer integer = essentialApiReceive.saveRepository(entityStream, AnnounceKindCode.PAIDINCREASE);
-
+        //그걸 EssentialResponseElement 리스트를 반환하는 ApiReceive 인터페이스 로직을 호출해서 구체 클래스로 반환
+        Stream<? extends EssentialReport> entityStream = essentialApiReceive.convertToEntity(elementStream, AnnounceKindCode.PAID_INCREASE);
+        //Mapper 인터페이스를 호출해서 가져옴
+        Integer integer = essentialApiReceive.saveRepository(entityStream, AnnounceKindCode.PAID_INCREASE);
+        //writer 인터페이스를 호출해서 적재
         return RepeatStatus.FINISHED;
     }
 }
